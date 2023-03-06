3f2dcaa24546fa3d83c98c37bef72fe391a4c278
Awes0meM4n <lonchezas@hotmail.com>
2023-03-06 08:52:00 +0100

    Reactividad profunda en objetos

--- frontend/src/components/SintaxisOptions.vue
+++ frontend/src/components/SintaxisOptions.vue
@@ -3,7 +3,9 @@ export default {
   data() {
     return {
       count: 1,
-      mensaje: "Hola mundo"
+      mensaje: "Hola mundo",
+      objeto: undefined,
+      array: [ { valor: 3 }, { valor: 2 } ]
     }
   },
 
@@ -14,15 +16,20 @@ export default {
     console.log(this.count) // => 1
     // data can be mutated as well
     this.count = 2
+
+    const objetoNuevo = { valor: 5 }
+    this.objeto = objetoNuevo
+    console.log("this.objeto es: ", this.objeto)
+    console.log("objetoNuevo es: ", objetoNuevo)
   }
 }
 </script>
 
 <template>
   <div>
-    Esto es el componente SintaxisOptions<br>
-    Valor de count es {{ count }}<br>
-    El mensaje es {{ mensaje }}<br>
-    <button type="button" @click="count++">count is {{ count }}</button><br>
+      Esto es el componente SintaxisOptions<br>
+      Valor de count es {{ count }}<br>
+      El mensaje es {{ mensaje }}<br>
+      <button type="button" @click="objeto.valor++">count is {{ objeto.valor }}</button><br>
   </div>
 </template>
