d56389ef51cca211b0545980ed60d450a896e653
Awes0meM4n <lonchezas@hotmail.com>
2023-03-13 10:54:27 +0100

    Aplicamos algo de estilo

--- frontend/src/components/practica-componentes/Participante.vue
+++ frontend/src/components/practica-componentes/Participante.vue
@@ -14,7 +14,8 @@ export default {
 
 <template>
   <div>
-    <div>{{ participante.nombre }} - ({{  participante.id }})</div>
-    <span @click="$emit('addGol')">Añadir gol</span> <span @click="addTarjeta">Añadir tarjeta</span>
+    <div class="d-inline me-2">{{ participante.nombre }} - <small class="text-muted">{{  participante.id }}</small></div>
+    <span class="badge bg-success me-2" @click="$emit('addGol')">Añadir gol</span>
+    <span class="badge bg-secondary" @click="addTarjeta">Añadir tarjeta</span>
   </div>
 </template>
