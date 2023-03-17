5910bbafeda2a5bda1c09bf914297ac0e2df7268
Awes0meM4n <lonchezas@hotmail.com>
2023-03-17 07:49:08 +0100

    Fontawesome añadido

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,8 +4,22 @@ import App from './App.vue'
 
 // Import our custom CSS
 import './scss/styles.scss'
-
 // Import all of Bootstrap's JS
 import * as bootstrap from 'bootstrap'
 
-createApp(App).mount('#app')
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+/* import specific icons */
+import { faFutbol, faSquareFull } from '@fortawesome/free-solid-svg-icons'
+/* add icons to the library */
+library.add(faFutbol, faSquareFull)
+
+const app = createApp(App)
+
+/* add font awesome icon component */
+app.component('font-awesome-icon', FontAwesomeIcon)
+
+app.mount('#app')
