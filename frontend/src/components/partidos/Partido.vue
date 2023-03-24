100bdbcb8c95c4005f530beef2d0f22f13daf1c7
Awes0meM4n <lonchezas@hotmail.com>
2023-03-24 12:04:05 +0100

    Calculamos los resultados con los sucesos

--- frontend/src/components/partidos/Partido.vue
+++ frontend/src/components/partidos/Partido.vue
@@ -14,10 +14,22 @@ export default {
     },
     fecha() {
       return dateToString(new Date(this.partido.timestamp))
+    },
+    goles() {
+      return this.partido.sucesos.goles
+    },
+    golesLocal() {
+      return this.golesParticipante(this.local)
+    },
+    golesVisitante() {
+      return this.golesParticipante(this.visitante)
     }
   },
   methods: {
     ...mapActions(participantesStore, [ 'getParticipantePorId' ]),
+    golesParticipante(participante) {
+      return this.goles.filter(g => g.idParticipante == participante.id).length
+    }
   }
 }
 </script>
@@ -26,6 +38,7 @@ export default {
   <div>
     <div>{{ fecha }}</div>
     <span>{{ local.nombre }}</span>
+    <span>{{ golesLocal }} - {{ golesVisitante }}</span>
     <span>{{ visitante.nombre }}</span>
   </div>
 </template>
