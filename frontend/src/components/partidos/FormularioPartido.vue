774945b96c8918acd17fc55b579d18100b0895bb
Awes0meM4n <lonchezas@hotmail.com>
2023-04-12 13:58:32 +0200

    FormularioPartido basico (selects participantes)
    * Se evitan poder elegir el mismo participante para local y visitante
    * Se crea un partido random con el modelo de la API

---
+++ frontend/src/components/partidos/FormularioPartido.vue
@@ -0,0 +1,73 @@
+<script>
+import { mapActions, mapState } from 'pinia'
+import { participantesStore } from '@/stores/participantes'
+
+function participanteRandom(participantes) {
+  return participantes[Math.floor(Math.random() * participantes.length)]
+}
+
+function prePartido(participantes) {
+  const local = participanteRandom(participantes)
+  let visitante = participanteRandom(participantes)
+  while (local == visitante) {
+    visitante = participanteRandom(participantes)
+  }
+
+  return { idLocal: local.id, idVisitante: visitante.id, timestamp: new Date().getTime() }
+}
+
+export default {
+  data() {
+    return {
+      partido: undefined
+    }
+  },
+  computed: {
+    ...mapState(participantesStore, [ 'participantes']),
+    local() {
+      return this.getParticipantePorId(this.partido.idLocal)
+    },
+    visitante() {
+      return this.getParticipantePorId(this.partido.idVisitante)
+    },
+    participantesLocal() {
+      return this.participantes.filter(p => p.id != this.visitante.id)
+    },
+    participantesVisitante() {
+      return this.participantes.filter(p => p.id != this.local.id)
+    }
+  },
+  methods: {
+    ...mapActions(participantesStore, [ 'getParticipantePorId' ]),
+  },
+  created() {
+    this.partido = this.partidoEditar ?? prePartido(this.participantes)
+  }
+}
+</script>
+
+<template>
+  <div>
+    <div>
+      <div>
+        <div class="input-group mb-3">
+          <span class="input-group-text" id="local">Local</span>
+          <select class="form-select" v-model="partido.idLocal">
+            <option v-for="participante of participantesLocal" :value="participante.id">{{ participante.nombre }}</option>
+          </select>
+        </div>
+
+        <div class="input-group mb-3">
+          <span class="input-group-text" id="visitante">Visitante</span>
+          <select class="form-select" v-model="partido.idVisitante">
+            <option v-for="participante of participantesVisitante" :value="participante.id">{{ participante.nombre }}</option>
+          </select>
+        </div>
+
+        <span class="fs-3 me-2">{{ local.nombre }}</span>
+        <span class="fs-3 me-2"> vs </span>
+        <span class="fs-3 me-2">{{ visitante.nombre }}</span>
+      </div>
+    </div>
+  </div>
+</template>
